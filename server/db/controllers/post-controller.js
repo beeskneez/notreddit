dd7c2cb15ea5e07acf487f3f128905d42eebe6ba
elaskowski <evalaskows@gmail.com>
2018-02-22 15:07:45 -0800

    Added gitignore file with postgres key & added controller/model folders

---
+++ server/db/controllers/post-controller.js
@@ -0,0 +1,48 @@
+const db = require('../db.js');
+const model = require('../models/post.js');
+
+// TODO: create model for these to do something with
+exports.getAll = (req, res) => {
+  model.post.findAll({}).then(function(posts) {
+    res.status(200).send(posts);
+  }, function(err) {
+    console.log(err);
+  })
+};
+
+exports.getOne = (req, res) => {
+  let id = req.params.id;
+  model.post.findOne({
+    where: {
+      id: id
+    }
+  }).then(function(post) {
+    res.status(200).send(post);
+  }, function(err) {
+      console.log(err);
+  });
+};
+
+exports.createOne = (req, res) => {
+  let title = req.body.title;
+  let body = req.body.body;
+  model.post.sync()
+    .then(function() {
+      return model.post.create({
+        uid: null,
+        title: title,
+        body: body
+      })
+    })
+    .then(function(post) {
+      res.status(200).send(post);
+    });
+};
+
+exports.updateOne = (req, res) => {
+  res.status(200).send('update one');
+};
+
+exports.deleteOne = (req, res) => {
+  res.status(200).send('delete one');
+};
--- server/routes.js
+++ server/routes.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const router = require('express').Router();
-const controller = require('./controller');
+// const controller = require('./controller');
 const path = require('path');
 
+const postController = require('./db/controllers/post-controller.js');
 
-router.route('/all').get(controller.getAll);
+router.route('/all').get(postController.getAll);
 router.route('/login');
 router.route('/signup');
 router.route('/subreddit');
-router.route('/post').post(controller.createOne);
+router.route('/post').post(postController.createOne);
 //New route created below
-router.route('/post').get(controller.getOne);
+router.route('/post').get(postController.getOne);
 // router.route('/:subreddit/post').post(controller.createOne);
 
 router.route('/user-history');
