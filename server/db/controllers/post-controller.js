5c0b5c8b1a2b5efaed94513e0da50c5db86d14a4
elaskowski <evalaskows@gmail.com>
2018-02-22 16:46:00 -0800

    (Refactor) Promisify Post model & create User model & controller  Please enter the commit message for your changes. Lines starting

--- server/db/controllers/post-controller.js
+++ server/db/controllers/post-controller.js
@@ -2,19 +2,19 @@ const db = require('../db.js');
 const model = require('../models/post.js');
 
 // TODO: create model for these to do something with
-exports.getAll = (req, res) => {
-  model.post.findAll({}).then(function(posts) {
+exports.getAllPosts = (req, res) => {
+  model.Post.findAll({}).then(function(posts) {
     res.status(200).send(posts);
   }, function(err) {
     console.log(err);
   })
 };
 
-exports.getOne = (req, res) => {
+exports.getPost = (req, res) => {
   let id = req.params.id;
-  model.post.findOne({
+  model.Post.findOne({
     where: {
-      uid: id
+      id: id
     }
   }).then(function(post) {
     res.status(200).send(post);
@@ -23,15 +23,20 @@ exports.getOne = (req, res) => {
   });
 };
 
-exports.createOne = (req, res) => {
+exports.createPost = (req, res) => {
   let title = req.body.title;
   let body = req.body.body;
-  model.post.sync()
+  let image = req.body.image;
+  model.Post.sync()
     .then(function() {
-      return model.post.create({
-        uid: null,
+      return model.Post.create({
+        id: null,
         title: title,
-        body: body
+        body: body,
+        likeCache: 0,
+        commentCache: 0,
+        image: image,
+        postType: 0
       })
     })
     .then(function(post) {
@@ -43,6 +48,18 @@ exports.updateOne = (req, res) => {
   res.status(200).send('update one');
 };
 
-exports.deleteOne = (req, res) => {
-  res.status(200).send('delete one');
+exports.deletePost = (req, res) => {
+  model.Post.destroy({
+    where: {
+      id: req.body.id
+    }
+  }).then(() => res.status(200).send('deleted'));
+  // res.status(200).send('delete one');
 };
+
+exports.deleteAllPosts = (req, res) => {
+  model.Post.destroy({
+    where: {},
+    truncate: true
+  }).then(() => res.send('deleted all'));
+}
