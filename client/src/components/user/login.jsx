06d8c00eb49bfcff19710c27aa826820d7fce8d8
Ryan Santos <ryansantos86@gmail.com>
2018-02-26 15:09:48 -0800

    [update] auth signin and login working

--- client/src/components/user/login.jsx
+++ client/src/components/user/login.jsx
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { auth } from 'firebase';
 
 export default class Login extends Component {
+  constructor() {
+    super();
+    this.login = this.login.bind(this);
+  }
+
+  login() {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .catch(e => console.error(e.message));
+    // auth.EmailAuthProvider();
+  }
+
   render() {
     return (
       <div className="outer">
@@ -10,15 +26,22 @@ export default class Login extends Component {
               <h2>Login</h2>
               <div className="two fields">
                 <div className="field">
-                  <label>username</label>
-                  <input placeholder="enter login username" type="text" />
+                  <label>email</label>
+                  <input id="email" placeholder="enter login email" type="text" />
                 </div>
                 <div className="field">
                   <label>password</label>
-                  <input placeholder="enter login password" type="text" />
+                  <input id="password" placeholder="enter login password" type="text" />
                 </div>
               </div>
-              <div className="ui submit button">Submit</div>
+              <div onClick={() => this.login()} className="ui submit button">
+                Submit
+              </div>
+              <div>
+                <small>
+                  don't have an account? <Link to="/signup">Sign up</Link>
+                </small>
+              </div>
             </div>
           </div>
         </div>
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,10 +1,16 @@
+// libs
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+// firebase
+import * as firebase from 'firebase';
+import FirebaseConfig from './../../environment/config';
+// components
 import App from './components/app.jsx';
 import allReducers from './reducers/index.jsx';
 
+firebase.initializeApp(FirebaseConfig);
 const store = createStore(allReducers);
 
 render(
