c7620fc38bd42532b2f3f7b3c04719843eef936b
elaskowski <evalaskows@gmail.com>
2018-02-27 15:52:11 -0800

    Staged

--- server/db/models/post.js
+++ server/db/models/post.js
@@ -5,7 +5,6 @@ const Subreddit = require('./subreddit.js');
 
 let Post;
 
-<<<<<<< HEAD
 const definePost = () => new Promise((resolve, reject) => {
   Post = db.define('post', {
     id: {
@@ -25,28 +24,6 @@ const definePost = () => new Promise((resolve, reject) => {
     id_parent: Sequelize.UUID,
     id_user: Sequelize.UUID,
     // id_sudreddit: Sequelize.UUID
-=======
-const definePost = () =>
-  new Promise((resolve, reject) => {
-    Post = db.define('post', {
-      id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-      },
-      title: Sequelize.STRING,
-      body: Sequelize.STRING,
-      // voteCache: Sequelize.INTEGER,
-      commentCache: Sequelize.INTEGER,
-      image: Sequelize.STRING,
-      postType: Sequelize.INTEGER,
-      // 0 is post, 1 is comment
-      id_parent: Sequelize.INTEGER,
-      id_user: Sequelize.INTEGER,
-      // id_sudreddit: Sequelize.INTEGER,
-      subreddit: Sequelize.STRING,
-    });
->>>>>>> [feat] Subreddit dropdown stylized and homepage rendering subreddits dynamically
   });
 
 definePost()
