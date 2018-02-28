c27462f084f0ea25495018c3c2f707ac45e35364
downier <raulishere@gmail.com>
2018-02-27 20:12:20 -0800

    (feat) Cleaning up after rebase

--- client/src/components/posts/postForm.jsx
+++ client/src/components/posts/postForm.jsx
@@ -14,46 +14,27 @@ class PostForm extends Component {
   }
 
   addNewPost() {
-<<<<<<< HEAD
     if (this.props.authUser) {
       this.state.user_email = this.props.authUser;
       axios
         .post('/post', { post: this.state })
-        .then(res => {
+        .then((res) => {
           this.props.createPost(res.data);
           axios
             .get('/posts')
-            .then(res => {
+            .then((res) => {
               this.props.updatePosts(res.data);
             })
-            .catch(err => {
+            .catch((err) => {
               console.error(err);
             });
         })
-        .catch(err => {
+        .catch((err) => {
           console.error(err);
         });
     } else {
       console.log('not logged in');
     }
-=======
-    axios
-      .post('/post', { post: this.state })
-      .then((res) => {
-        this.props.createPost(res.data);
-        axios
-          .get('/posts')
-          .then((res) => {
-            this.props.updatePosts(res.data);
-          })
-          .catch((err) => {
-            console.error(err);
-          });
-      })
-      .catch((err) => {
-        console.error(err);
-      });
->>>>>>> (feat) working on store passing props
   }
 
   onChange(e) {
@@ -64,38 +45,55 @@ class PostForm extends Component {
   }
 
   render() {
-<<<<<<< HEAD
-    return <div className="outer">
-=======
-    console.log('our props', this.props.authUser);
     return (
       <div className="outer">
->>>>>>> (feat) working on store passing props
         <div className="middle">
           <div className="inner">
             <div className="ui huge form">
               <h2>Create New Post</h2>
               <div className="field">
                 <label>title</label>
-                <input name="title" placeholder="enter post title" type="text" onChange={e => this.onChange(e)} />
+                <input
+                  name="title"
+                  placeholder="enter post title"
+                  type="text"
+                  onChange={e => this.onChange(e)}
+                />
               </div>
               <div className="field">
                 <label>body</label>
-                <textarea name="body" placeholder="enter post body" type="text" onChange={e => this.onChange(e)} />
+                <textarea
+                  name="body"
+                  placeholder="enter post body"
+                  type="text"
+                  onChange={e => this.onChange(e)}
+                />
               </div>
               <div className="field">
                 <label>image URL</label>
-                <input name="image" placeholder="enter image URL" type="text" onChange={e => this.onChange(e)} />
+                <input
+                  name="image"
+                  placeholder="enter image URL"
+                  type="text"
+                  onChange={e => this.onChange(e)}
+                />
               </div>
             </div>
             <div>
               <SubredditList onChange={e => this.onChange(e)} />
             </div>
-            {this.props.authUser 
-              ? <Link className="ui submit button" onClick={this.addNewPost} to="/"> Submit </Link> : 'Must be logged in to submit!'}
+            {this.props.authUser ? (
+              <Link className="ui submit button" onClick={this.addNewPost} to="/">
+                {' '}
+                Submit{' '}
+              </Link>
+            ) : (
+              'Must be logged in to submit!'
+            )}
           </div>
         </div>
-      </div>;
+      </div>
+    );
   }
 }
 // }
@@ -103,11 +101,7 @@ class PostForm extends Component {
 function mapStateToProps(state) {
   return {
     post: state.post,
-<<<<<<< HEAD
-    authUser: state.authUser
-=======
     authUser: state.authUser,
->>>>>>> (feat) working on store passing props
   };
 }
 
