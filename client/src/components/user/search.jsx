9d2e613933bbfb1cd7d3fe00e8586bd70f106e98
downier <raulishere@gmail.com>
2018-03-01 12:40:27 -0800

    (feat) Saving prior to rebase

--- client/src/components/user/search.jsx
+++ client/src/components/user/search.jsx
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Search extends Component {
   render() {
-    return <h1>Search</h1>;
+    return (
+      <div className="outer">
+        <div className="middle">
+          <div className="inner">
+            <div className="ui huge form">
+              <h2>Search</h2>
+              <div className="two fields">
+                <div className="field">
+                  <label>Search post titles:</label>
+                  <input id="email" placeholder="enter login email" type="text" />
+                </div>
+              </div>
+              <div onClick={() => console.log('clicked')} className="ui submit button">
+                Search
+              </div>
+              <div>
+                <small>
+                  Back to notreddit? <Link to="/">Main feed</Link>
+                </small>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
