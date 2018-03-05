94036610de6adc3ceada13310e56bdc3b4cfaec8
downier <raulishere@gmail.com>
2018-03-05 15:13:40 -0800

    (feat) Corrected console err

--- client/src/reducers/userReducer.jsx
+++ client/src/reducers/userReducer.jsx
@@ -1,15 +1,15 @@
 export function ReducerUser(state = null, action) {
   switch (action.type) {
-    case 'SET_USER':
+    case "SET_USER":
       return action.payload;
     default:
       return state;
   }
 }
 
-export function ReducerUserSubscriptionList(state = null, action) {
+export function ReducerUserSubscriptionList(state = [], action) {
   switch (action.type) {
-    case 'GET_USER_SUBSCRIPTION_LIST':
+    case "GET_USER_SUBSCRIPTION_LIST":
       return action.payload;
     default:
       return state;
