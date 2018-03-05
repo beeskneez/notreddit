91e91a6a176fad58ef33e775cd74a1b180b1409e
Ryan Santos <ryansantos86@gmail.com>
2018-03-05 14:25:06 -0800

    [refactor] details voting works

--- client/src/components/user/tabs/new.jsx
+++ client/src/components/user/tabs/new.jsx
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { Link } from 'react-router-dom';
 import axios from 'axios';
-import moment from 'moment';
 
 import { getPost } from './../../../actions/index.jsx';
+import NewListEntry from './newListEntry.jsx';
 
 class New extends Component {
   constructor() {
@@ -33,11 +32,6 @@ class New extends Component {
     });
   }
 
-  goToDetails(post) {
-    this.props.getPost(post);
-    this.props.history.push(`/postDetails/${post.id}`);
-  }
-
   render() {
     const sortedPosts = this.state.posts.sort((a, b) => {
       return new Date(b.createdAt) - new Date(a.createdAt);
@@ -48,37 +42,11 @@ class New extends Component {
         <h2 className="ui header">newest posts in your subscriptions</h2>
         {sortedPosts.map((post, index) => {
           return (
-            <div key={index} className="twelve wide column">
-              <img className="thumbnail" src={post.image} alt="" />
-              <a
-                onClick={() => this.goToDetails(post)}
-                className="ui large header"
-              >
-                {post.title}
-              </a>
-              <div className="meta">
-                submitted {moment(post.createdAt).format('ddd, h:mmA')} ago by{' '}
-                <a>{post.username}</a> to{' '}
-                <Link to={`/subreddit/${post.subreddit}`}>
-                  /{post.subreddit}
-                </Link>
-              </div>
-              <ul className="ui big horizontal list voters">
-                <li className="item">
-                  <a>
-                    <i className="arrow up icon" />
-                    upvote
-                  </a>
-                </li>
-                <li className="item">{post.votes}</li>
-                <li className="item">
-                  <a>
-                    <i className="arrow down icon" />
-                    downvote
-                  </a>
-                </li>
-              </ul>
-            </div>
+            <NewListEntry
+              key={index}
+              post={post}
+              history={this.props.history}
+            />
           );
         })}
       </div>
